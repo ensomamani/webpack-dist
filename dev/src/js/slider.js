a1134bb5191e5f6ec811a40a931087569aa631e1
Enso <ensoman.20@gmail.com>
2018-03-25 15:00:51 -0500

    animacion tipo slider a juegos

--- dev/src/js/slider.js
+++ dev/src/js/slider.js
@@ -1,18 +1,58 @@
 (() => {
-   
+    // variable slider del html
     let slide = document.getElementById('slider');
 
-    /* Funcion para que el slider tenga animacion */
-    let counter = 0; 
-    let moveSlide = () => {
-        counter += -250;
-        slide.style.left = ` ${counter}px `;
-        console.log(counter);
-        if (counter === -750) {
-            counter = 0
+    class animationSlider {
+        constructor(counter, increment) {
+            this._counter = counter;
+            this._increment = increment;
         }
+
+        // getter y setter de counter
+        set setCounter(counter) {
+            this._counter = counter;
+        }
+        get getCounter() {
+            return this._counter;
+        }
+
+        // getter y setter de increment
+        set setIncrement(increment) {
+            this._increment = increment
+        }
+        get getIncrement() {
+            return this._increment;
+        }
+
+        //Métodos
+        moveSlide() {
+            setInterval(() => {
+                this.setCounter = this.getCounter + this.getIncrement;
+                if(this.getCounter === -1000) {
+                    this.setCounter = 0;
+                }
+                slide.style.left = `${this.getCounter}px`;
+            }, 3000)
+        }
+
     }
+
+    // /* Funcion para que el slider tenga animacion */
+    // let counter = 0; 
+    // let moveSlide = () => {
+    //     counter += -250;
+    //     slide.style.left = ` ${counter}px `;
+    //     if (counter === -750) {
+    //         counter = 250;
+    //     }
+        
+    //     console.log(counter);
+    // }
+    // setInterval(moveSlide, 3000);
+
+    // // indicamos cuanto tiempo tendrá la animación
+    const animation = new animationSlider(0, -250);
+    animation.moveSlide();
     
-    setInterval( moveSlide, 5000);
 })();
 
