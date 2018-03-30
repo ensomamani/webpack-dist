63b80bde956f3dd0e19f80705cbd2400ad4d9386
Enso <ensoman.20@gmail.com>
2018-03-30 12:46:34 -0500

    detalles arreglados

--- dev/src/js/app.js
+++ dev/src/js/app.js
@@ -2,6 +2,7 @@
 /* Programacion */ 
 // const pug = require('../templates/index.pug');
 const scss = require('../scss/style.scss');//se encuentra fuera de dist para separarlo del css 
+
 //const fonts = require('./fontawesome-all.js');
 const fonts = require('../fonts/style/fontello.css');
 
--- dev/src/js/slider.js
+++ dev/src/js/slider.js
@@ -1,58 +1,11 @@
+import animationSlider from './clases/animationSlider.js';
 (() => {
     // variable slider del html
     let slide = document.getElementById('slider');
 
-    class animationSlider {
-        constructor(counter, increment) {
-            this._counter = counter;
-            this._increment = increment;
-        }
-
-        // getter y setter de counter
-        set setCounter(counter) {
-            this._counter = counter;
-        }
-        get getCounter() {
-            return this._counter;
-        }
-
-        // getter y setter de increment
-        set setIncrement(increment) {
-            this._increment = increment
-        }
-        get getIncrement() {
-            return this._increment;
-        }
-
-        //Métodos
-        moveSlide() {
-            setInterval(() => {
-                this.setCounter = this.getCounter + this.getIncrement;
-                if(this.getCounter === -1000) {
-                    this.setCounter = 0;
-                }
-                slide.style.left = `${this.getCounter}px`;
-            }, 3000)
-        }
-
-    }
-
-    // /* Funcion para que el slider tenga animacion */
-    // let counter = 0; 
-    // let moveSlide = () => {
-    //     counter += -250;
-    //     slide.style.left = ` ${counter}px `;
-    //     if (counter === -750) {
-    //         counter = 250;
-    //     }
-        
-    //     console.log(counter);
-    // }
-    // setInterval(moveSlide, 3000);
-
-    // // indicamos cuanto tiempo tendrá la animación
-    const animation = new animationSlider(0, -250);
-    animation.moveSlide();
-    
+    /* Instancia de animacion */ 
+    let animation = new animationSlider(0, -250);
+    animation.moveSlide(slide);
+    animationSlider.hola();
 })();
 
